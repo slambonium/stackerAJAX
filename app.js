f8bfe3800c397b2aec66e81e29b5756c854613a6
Robert T. Stewart <bobstewart@goodtiming.com>
2015-04-29 09:48:48 -0400

    assignment requirements met

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 $(document).ready( function() {
+
+	/* http://api.stackexchange.com/ */
+
+	/* http://api.stackexchange.com/docs/unanswered-questions */
+
+	/* http://api.stackexchange.com/docs/top-answerers-on-tags */
+
 	$('.unanswered-getter').submit( function(event){
 		// zero out results if previous search has run
 		$('.results').html('');
@@ -6,13 +13,23 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
-});
+
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tag the user submitted
+		var tag = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(tag);
+	});
+
+}); /* end $(document).ready(function() {...}) */
 
 // this function takes the question object returned by StackOverflow 
 // and creates new result to be appended to DOM
 var showQuestion = function(question) {
 	
 	// clone our result template code
+	/* http://api.jquery.com/clone/ */
 	var result = $('.templates .question').clone();
 	
 	// Set the question properties in result
@@ -39,7 +56,32 @@ var showQuestion = function(question) {
 	);
 
 	return result;
-};
+}; /* end function showQuestion() */
+
+// this function takes the user object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showAnswerer = function(user) {
+	
+	// clone our result template code
+	/* http://api.jquery.com/clone/ */
+	var result = $('.templates .answerer').clone();
+	
+	// Set the answerer-info properties in result
+
+	// set href value
+	result.find('.answerer-info a').attr('href', user.user.link);
+  // assign image url
+  result.find('img').attr('src', user.user.profile_image);
+  // assign figcaption contents
+  result.find('figcaption').text(user.user.display_name);
+
+  // assign score property 
+  result.find('.score').text(user.score);
+  // assign reputation property 
+  result.find('.reputation').text(user.user.reputation);  
+
+	return result;
+}; /* end function showAnswerer() */
 
 
 // this function takes the results object from StackOverflow
@@ -56,9 +98,23 @@ var showError = function(error){
 	errorElem.append(errorText);
 };
 
-// takes a string of semi-colon separated tags to be searched
-// for on StackOverflow
+/*
+
+getUnanswered function calls the other functions in the middle of the file. getUnanswered first creates an object that contains the parameters which will be passed in the GET request to the StackExchange API.
+
+Next, a variable is created whose value is a deferred object (this is the underlying object returned by the $.ajax() method). In the call to .ajax(), for the url setting, the endpoint from step 3 gets passed. For the data key, you pass in the requestParams object you created above. Since you want a client-side cross-domain request to this API, you must specify “jsonp” (http://bob.ippoli.to/archives/2005/12/05/remote-json-jsonp/) for the dataType setting. Finally, set the method to “GET” since this is the HTTP method used.
+
+The logic in the .done() and .fail() blocks is similar. Remember that the .done() block will only execute when and if the AJAX returns successfully. The .fail() block will only execute when and if the AJAX request fails. In both cases, the strategy (looking at the code for the showQuestion, showSearchResults and showError functions) is to .clone() - http://api.jquery.com/clone/ - the appropriate hidden DOM element, insert new text and HTML into the clone based on our response data, then insert this cloned element into the visible .container div.
+
+This is a simple design pattern that you can use again and again when you’re prototyping a AJAX-based web page. If you were building a full-fledged application, you would probably want to start thinking about storing your templates as partials that you could also load using AJAX. This would make your app more modular.
+
+There are some additional implementation details in the showQuestion, showSearchResults, and showError functions that you should review on your own. In the next assignment, you will take the principles demonstrated here and apply them to building a second feature for this web page.
+
+*/
+
 var getUnanswered = function(tags) {
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow	
 	
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = {tagged: tags,
@@ -67,9 +123,12 @@ var getUnanswered = function(tags) {
 								sort: 'creation'};
 	
 	var result = $.ajax({
+		/* http://api.jquery.com/jQuery.ajax/ */
+		/* http://api.stackexchange.com/docs/unanswered-questions */
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
 		dataType: "jsonp",
+		/* http://bob.ippoli.to/archives/2005/12/05/remote-json-jsonp/ */
 		type: "GET",
 		})
 	.done(function(result){
@@ -86,7 +145,52 @@ var getUnanswered = function(tags) {
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
-};
+
+}; /* end function getUnanswered() */
+
+var getTopAnswerers = function(tag) {
+// takes a string of one tag to be searched
+// for on StackOverflow	
+
+  /* sample: http://api.stackexchange.com/2.2/tags/php/top-answerers/all_time?site=stackoverflow&dataType=text&type=GET */
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	/* tags/{tag}/top-answerers/{period} : http://api.stackexchange.com/docs/types/tag-score */
+	var request = {tag: tag + '/top-answerers/all_time',
+		            // period: 'all_time',
+		            //period: 'month',
+								site: 'stackoverflow',
+					      };
+	
+	var result = $.ajax({
+		/* http://api.jquery.com/jQuery.ajax/ */
+		/* http://api.stackexchange.com/docs/unanswered-questions */
+		// url: "http://api.stackexchange.com/2.2/tags/",
+		url: "http://api.stackexchange.com/2.2/tags/" + tag + '/top-answerers/all_time?site=stackoverflow',
+		// data: request,
+		// data: tag + '/top-answerers/all_time/site=stackoverflow',
+		dataType: "jsonp",
+		/* http://bob.ippoli.to/archives/2005/12/05/remote-json-jsonp/ */
+		type: "GET",
+		})
+	.done(function(result){
+		// var searchResults = showSearchResults(request.tag, result.items.length);
+		var searchResults = showSearchResults(tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+
+}; /* end function getTopAnswerers() */	
+
 
 
 
